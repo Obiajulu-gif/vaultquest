df02c721e18977ef16e7aa0ba2d9725e274c87a5
barry01_hash <henryebube62@gmail.com>
2026-06-23 13:31:38 +0100

    end-to-end core flows

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@vaultquest/stellar-wallet-connect"],
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@react-native-async-storage/async-storage": path.resolve(
+        "./lib/shims/async-storage.js",
+      ),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
